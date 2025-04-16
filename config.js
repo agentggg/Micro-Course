810c1f878e3def20a9449b538ce9b7598b2babc2
Your Name <your-email@example.com>
2025-04-16 19:38:37 -0400

    automated upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const CONFIG = {
+    // API_BASE: "https://default-backend-4ga4.onrender.com",
+    API_BASE: "http://localhost:3000",
+    REDIRECT_AFTER_SUBSCRIBE: "https://your-redirect-site.com"
+};
